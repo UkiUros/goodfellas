0e582a2684a573ff50f039a5531a4d94703273be
urossimic <scientific1>
2016-05-28 05:35:39 +0200

    Main screen icons

--- app/src/main/java/rs/forexample/goodfellas/MainActivity.java
+++ app/src/main/java/rs/forexample/goodfellas/MainActivity.java
@@ -1,28 +1,39 @@
 package rs.forexample.goodfellas;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+
+import com.joanzapata.iconify.IconDrawable;
+import com.joanzapata.iconify.fonts.IoniconsIcons;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View btnScan;
+    private View btnShop;
+    private ImageView imgScan;
+    private ImageView imgShop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button scan = (Button) findViewById(R.id.button);
-
-        scan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnScan = findViewById(R.id.btnScan);
+        btnShop = findViewById(R.id.btnShop);
+        initializeBottomIcons();
 
 
-            }
-        });
 
+
+    }
+
+    private void initializeBottomIcons(){
+        imgScan = (ImageView) findViewById(R.id.imgScan);
+        imgShop = (ImageView) findViewById(R.id.imgShop);
+
+        imgScan.setImageDrawable(new IconDrawable(getApplicationContext(), IoniconsIcons.ion_qr_scanner).sizeDp(30).colorRes(R.color.colorWhite));
+        imgScan.setImageDrawable(new IconDrawable(getApplicationContext(), IoniconsIcons.ion_card).sizeDp(30).colorRes(R.color.colorWhite));
     }
 }
