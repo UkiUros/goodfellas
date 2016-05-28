24fed8c900df0e3310eceeeae49bcd88f470bd6c
Dejan Petkovic <dejanpetko@hotmail.com>
2016-05-28 20:30:08 +0200

    permium pictures added

---
+++ app/src/main/java/rs/forexample/goodfellas/adapter/PremiumImageAdapter.java
@@ -0,0 +1,63 @@
+package rs.forexample.goodfellas.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import rs.forexample.goodfellas.R;
+
+/**
+ * Created by deki on 28.5.16..
+ */
+public class PremiumImageAdapter extends RecyclerView.Adapter {
+
+    public interface OnImageClickedListener {
+        void onImageClicked(int image);
+    }
+
+    ArrayList<Integer> mImages;
+    OnImageClickedListener mOnImageClickedListener;
+
+    public PremiumImageAdapter(ArrayList<Integer> imageList, OnImageClickedListener onImageClickedListener)
+    {
+        mImages = imageList;
+        mOnImageClickedListener = onImageClickedListener;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ImageViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_premium_image, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int imageId = mImages.get(position);
+        ((ImageViewHolder) holder).imageView.setImageResource(imageId);
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return mImages.size();
+    }
+
+    private class ImageViewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView imageView;
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mOnImageClickedListener.onImageClicked(mImages.get(getAdapterPosition()));
+                }
+            });
+        }
+    }
+}
